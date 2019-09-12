9c95bbe340e0182d4e183505b7b97fdd6f14a2cf
jinmokim <jinmokim@218.38.137.27>
2019-09-12 16:05:33 +0900

    test

--- SmartTeaching/src/main/java/com/team/Controll/HomeController.java
+++ SmartTeaching/src/main/java/com/team/Controll/HomeController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HomeController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {	
+		System.out.println("kim");
 		return "index";
 	}
 	@RequestMapping(value = "/404", method = RequestMethod.GET)
